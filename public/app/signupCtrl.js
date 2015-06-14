7f27e387463edee3788bed6294ac836989ca2d43
ThangTheMan <thang@fromttm.com>
2015-06-13 23:01:26 -0700

    form clear
    form clear after submit button hit

--- public/app/signupCtrl.js
+++ public/app/signupCtrl.js
@@ -1,7 +1,12 @@
 myApp.controller("signupCtrl", function($scope, notifier, $location, UserService){
 
     $scope.signup = function(){
-        UserService.createUser($scope.email,$scope.password)
+        UserService.createUser($scope.email,$scope.password);
+        $scope.email=null;
+        $scope.password=null;
+        $scope.fname = null;
+        $scope.lname =null;
+        console.log('form clear after submit');
     };
 
 });
